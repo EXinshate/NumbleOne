5d1e08610c65fa58b06b71a0905cff555a1c1ae1
Mylittleairport2333 <758285009@qq.com>
2023-01-29 18:43:47 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+ public class Main {
+        public static void main(String[] args) {
+            int myvar1;
+            int myvar2;
+
+            myvar1 = 1024;
+            System.out.println("myVar1 contains " + myvar1);
+
+            myvar2 = myvar1 / 2;
+
+            System.out.print("myV                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ar2 contains myVar1 / 2: ");
+            System.out.println(myvar2);
+
+        }
+    }
